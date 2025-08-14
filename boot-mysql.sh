66a9f83dc0d778203046d9f7426d77e368db4c2c
0xLS <zerodna1@gmail.com>
2025-08-14 08:18:22 +0200

    fix timezone

--- boot-mysql.sh
+++ boot-mysql.sh
@@ -1,8 +1,6 @@
 docker run -d \
   --name mysql \
   -e MYSQL_ROOT_PASSWORD=password \
-  -e TZ=Europe/Copenhagen \
   -p 0.0.0.0:3306:3306 \
   --restart unless-stopped \
   mysql:8.0 \
-  --default-time-zone='Europe/Copenhagen'
